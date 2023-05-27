d4073b8fa0c25873cac34dc0aee204f419833e75
mulchus <mulch@list.ru>
2023-05-27 17:56:44 +0300

    fix bug with timezone Moscow +2.30. USE_TZ = False

--- bot_beautycity/settings.py
+++ bot_beautycity/settings.py
@@ -119,7 +119,7 @@ USE_I18N = True
 USE_THOUSAND_SEPARATOR = True
 THOUSAND_SEPARATOR = '&nbsp;'
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
